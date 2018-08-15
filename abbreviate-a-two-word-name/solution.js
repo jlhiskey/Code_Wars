c76e0a1fdce82fe7fa2ece36b1aa243482eee486
jlhiskey <jasonhi@crazyredhead.com>
2018-08-15 12:13:15 -0700

    completed my head is at the wrong end

--- abbreviate-a-two-word-name/solution.js
+++ abbreviate-a-two-word-name/solution.js
@@ -2,7 +2,5 @@
 
 function abbrevName(name){
   let splitName = name.split(' ');
-  let firstInitial = splitName [0][0].toUpperCase();
-  let lastInitial = splitName [1][0].toUpperCase();
-  return `${firstInitial}.${lastInitial}`;
+  return `${splitName [0][0].toUpperCase()}.${splitName [1][0].toUpperCase()}`;
 }
