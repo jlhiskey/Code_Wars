766d54936ef28beb5993be8a3ce85e8a21d34d8e
jlhiskey <jasonhi@crazyredhead.com>
2018-08-22 09:12:07 -0700

    completed disenvowel-trolls

--- insert-dashes/solution.js
+++ insert-dashes/solution.js
@@ -2,7 +2,7 @@
 function insertDash(num) {
   let stringify = String(num);
   let withDash = '';
-  for (let i = 0; i < stringify.length; i++) {
+   (let i = 0; i < stringify.length; i++) {
     withDash += stringify.charAt(i);
     if ((Number(stringify.charAt(i)) % 2 === 1) && (Number(stringify.charAt(i + 1)) % 2 === 1)) {
       withDash += '-';
--- nice-array/solution.js
+++ nice-array/solution.js
@@ -1,7 +1,6 @@
 'use strict';
 
 function isNice(arr) {
-  console.log(arr);
   let value = false;
   if (arr.length === 0 || arr.length === 1) {
     return value;
@@ -9,15 +8,10 @@ function isNice(arr) {
   else {
     for (let i = 0; i < arr.length; i++) {
       if (arr.includes(arr[i] + 1) || arr.includes(arr[i] - 1)) {
-        console.log('i=',i);
-        console.log('i+1',arr.includes(arr[i] + 1));
-        console.log('i-1',arr.includes(arr[i] - 1));
-        console.log('value=',value);
         value = true;
       }
       else {
-        value = false;
-        console.log('else fired');
+        return false;
 
       }
     }
